ad80522418c54316c178e37d3eca340720c6b400
Shah Fazle Elahi <shahfazleelahi@Shah-Fazle-Elahi.local>
2024-07-28 07:20:30 +0600

    besic Home ui

--- quiz_app/client/src/components/main.js
+++ quiz_app/client/src/components/main.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import '../styles/Main.css'
 
 export default function Main() {
-    return(
+
+    const inputRef = useRef(null)
+
+    return (
         <div className="container">
-            <h1> Quiz Application</h1>
+            <h1 className="title text-light"> Quiz Application</h1>
+
+            <ol>
+                <li>You will be asked 10 questions one after anthor.</li>
+                <li>10 point is awarded for the correct answer.</li>
+                <li>Each Question has three option. You can choose only one option.</li>
+                <li>You can review and change answers before the quiz finish.</li>
+                <li>The result will be declared at the end of the quiz.</li>
+            </ol>
+
+            <form id="form">
+                <input ref={inputRef} type="text" placeholder='Username*'></input>
+            </form>
+
+            <div className="start">
+                <Link className="btn" to={'quiz'}>Start Quiz</Link>
+            </div>
         </div>
     )
 }
